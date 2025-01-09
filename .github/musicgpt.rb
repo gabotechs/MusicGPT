9cc632dae1c9b4942d1257e0962e28e5fec5868f
gabotechs <gb.mt.me@gmail.com>
2025-01-09 14:04:23 +0100

    Streamline release process

--- .github/musicgpt.rb
+++ .github/musicgpt.rb
@@ -5,37 +5,36 @@ class Musicgpt < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gabotechs/MusicGPT/releases/download/v<version>/aarch64-apple-darwin.tar.gz"
+      url "https://github.com/gabotechs/MusicGPT/releases/download/v<version>/musicgpt-aarch64-apple-darwin"
 
       def install
-        bin.install "musicgpt"
+        bin.install "musicgpt-aarch64-apple-darwin" => "musicgpt"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/gabotechs/MusicGPT/releases/download/v<version>/x86_64-apple-darwin.tar.gz"
+      url "https://github.com/gabotechs/MusicGPT/releases/download/v<version>/musicgpt-x86_64-apple-darwin"
 
       def install
-        bin.install "musicgpt"
+        bin.install "musicgpt-x86_64-apple-darwin" => "musicgpt"
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/gabotechs/MusicGPT/releases/download/v<version>/x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/gabotechs/MusicGPT/releases/download/v<version>/musicgpt-x86_64-unknown-linux-gnu"
 
       def install
-        bin.install "musicgpt"
-        lib.install Dir["lib/*"]
+        bin.install "musicgpt-x86_64-unknown-linux-gnu" => "musicgpt"
       end
     end
-# TODO: Linux ARM still does not work.
-#     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-#       url "https://github.com/gabotechs/MusicGPT/releases/download/v<version>/musicgpt-aarch64-unknown-linux-gnu.tar.gz"
-#
-#       def install
-#         bin.install "musicgpt"
-#       end
-#     end
+    # TODO: Linux ARM still does not work.
+    # if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    #   url "https://github.com/gabotechs/MusicGPT/releases/download/v<version>/musicgpt-aarch64-unknown-linux-gnu"
+    #
+    #   def install
+    #     bin.install "musicgpt-aarch64-unknown-linux-gnu" => "musicgpt"
+    #   end
+    # end
   end
 end
